fcc55d3c0d56a58a16a3e02a44bc8b5e84bd9d64
Roman Yaurov <yaurov.r@gmail.com>
2024-12-18 17:41:53 +0300

    [jwt.util.ts] fixed bug

--- src/utils/jwt.util.ts
+++ src/utils/jwt.util.ts
@@ -1,26 +1,41 @@
 import jwt from 'jsonwebtoken';
 
-const accessSecret = process.env.ACCESS_TOKEN_SECRET!;
-const refreshSecret = process.env.REFRESH_TOKEN_SECRET!;
-
 export const generateAccessToken = (userId: string) => {
+  const accessSecret = process.env.ACCESS_TOKEN_SECRET;
+
+  if (!accessSecret)
+    throw new Error('ACCESS_TOKEN_SECRET is not defined in .env');
+
   return jwt.sign({ userId }, accessSecret, {
-    expiresIn: process.env.ACCESS_TOKEN_EXPIRATION,
+    expiresIn: '15m',
   });
 };
 
 export const generateRefreshToken = (userId: string) => {
+  const refreshSecret = process.env.REFRESH_TOKEN_SECRET;
+
+  if (!refreshSecret)
+    throw new Error('REFRESH_TOKEN_SECRET is not defined in .env');
+
   return jwt.sign({ userId }, refreshSecret, {
-    expiresIn: process.env.REFRESH_TOKEN_EXPIRATION,
+    expiresIn: '5d',
   });
 };
 
 export const verifyAccessToken = (token: string): { userId: string } => {
-  console.log(accessSecret);
-  console.log(refreshSecret);
+  const accessSecret = process.env.ACCESS_TOKEN_SECRET;
+
+  if (!accessSecret)
+    throw new Error('ACCESS_TOKEN_SECRET is not defined in .env');
+
   return jwt.verify(token, accessSecret) as { userId: string };
 };
 
 export const verifyRefreshToken = (token: string): { userId: string } => {
+  const refreshSecret = process.env.REFRESH_TOKEN_SECRET;
+
+  if (!refreshSecret)
+    throw new Error('REFRESH_TOKEN_SECRET is not defined in .env');
+
   return jwt.verify(token, refreshSecret) as { userId: string };
 };
